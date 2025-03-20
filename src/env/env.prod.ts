b55476a5e9f337e248282e2fd07ae5e3fbb728bc
JoLu <94259703+JoLuuMtz@users.noreply.github.com>
2025-03-19 21:19:23 -0500

    Delete src/env

--- src/env/env.prod.ts
+++
@@ -1,8 +0,0 @@
-
-export const env = {
-
-  producccion: true,
-  API: 'http://localhost:3000',
-  API_KEY: '123456789'
-
-};
--- src/env/env.ts
+++
@@ -1,5 +0,0 @@
-export const env = {
-
-  API: 'http://localhost:3000',
-
-};
